e1fcfd1254ea610bb2f67e32b5b8aa6b9f709088
dyjhor014 <jordycs93@gmail.com>
2023-09-10 22:47:47 -0500

    create model, schema, router of type_group and create method get, post

---
+++ config/database.py
@@ -0,0 +1,16 @@
+# database.py
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = "mysql://root:asdfghjkl@localhost/blog_viajeroz"
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
