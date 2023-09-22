9f5ea080503b950f7c9f4a97d70e3dfff8de6d84
dyjhor014 <jordycs93@gmail.com>
2023-09-22 15:33:15 -0500

    add hashed password for user

--- routers/user_router.py
+++ routers/user_router.py
@@ -1,3 +1,4 @@
+import bcrypt
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from config.database import get_db
@@ -13,8 +14,22 @@ def get_all_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)
 
 @router.post("/user", response_model=UserBase)
 def create_user(user: UserCreate, db: Session = Depends(get_db)):
+    # Obtener la contraseña sin hashear desde el objeto UserCreate
+    password = user.password.encode('utf-8')  # Codifica la contraseña a bytes
+    
+    # Hashea la contraseña
+    hashed_password = bcrypt.hashpw(password, bcrypt.gensalt())
+    
+    # Crea un nuevo objeto User con la contraseña hasheada
     new_user = User(**user.dict())
+    
+    # Asigna la contraseña hasheada al campo password
+    new_user.password = hashed_password.decode('utf-8')
+    
+    # Agrega el nuevo usuario a la base de datos
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
+    
     return new_user
+
