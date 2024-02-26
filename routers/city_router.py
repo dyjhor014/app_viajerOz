c862e4c2a902d47494bfbb020103fb2e46fd63ef
dyjhor014 <jordycs93@gmail.com>
2024-02-26 16:23:58 -0500

    add endpoint to find city by name

--- routers/city_router.py
+++ routers/city_router.py
@@ -3,16 +3,27 @@ from sqlalchemy.orm import Session
 from config.database import get_db
 from models.models import City
 from schemas.city import CityBase, CityCreate, CityList, CityUpdate
+from decorators.roles.role_verify import role_required
+from auth.auth import get_token_from_request
 
 router = APIRouter()
 
 @router.get("/city", response_model=CityList)
-def get_all_cities(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+#@role_required(["admin"])
+async def get_all_cities(token: str = Depends(get_token_from_request), skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     cities = db.query(City).offset(skip).limit(limit).all()
     return {"cities": cities}
 
+@router.get("/city/find_by_name/{name}", response_model=CityList)
+@role_required(["admin", "user", "moderator"])
+async def get_all_cities(name: str, token: str = Depends(get_token_from_request), skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    cities = db.query(City).filter(City.name.like("%"+name+"%")).offset(skip).limit(limit).all()
+    if  not cities: 
+        raise HTTPException(status_code=404, detail="Not Found")
+    return {"cities": cities}
+
 @router.post("/city", response_model=CityBase)
-def create_city(city: CityCreate, db: Session = Depends(get_db)):
+async def create_city(city: CityCreate, db: Session = Depends(get_db)):
     new_city = City(**city.model_dump())
     db.add(new_city)
     db.commit()
